9ffe05ae539c67814965c2c1ba05b56289925bf0
Damien Robinson <damien.robinson@daim.dev>
2021-02-14 12:37:07 +1000

    chore(layout): add error pages

---
+++ components/error/404.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card id="message">
+    <h2>404</h2>
+    <h1>Page not found</h1>
+    <v-btn to="/index" color="primary" block large>Home</v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  head() {
+    return {
+      title: 'Page not found',
+      meta: [],
+    }
+  },
+}
+</script>
+
+<style media="screen" scoped>
+#message {
+  max-width: 360px;
+  margin: 100px auto 16px;
+  padding: 32px 24px 16px;
+  border-radius: 3px;
+}
+#message h3 {
+  font-weight: normal;
+  font-size: 16px;
+  margin: 16px 0 12px;
+}
+#message h2 {
+  font-weight: bold;
+  font-size: 16px;
+  margin: 0 0 8px;
+}
+#message h1 {
+  font-size: 22px;
+  font-weight: 300;
+  margin: 0 0 16px;
+}
+#message p {
+  line-height: 140%;
+  margin: 16px 0 24px;
+  font-size: 14px;
+}
+#load {
+  color: rgba(0, 0, 0, 0.4);
+  text-align: center;
+  font-size: 13px;
+}
+</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,42 +1,28 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+  <div class="nuxt-error container">
+    <component :is="errorPage" :error="error" />
+  </div>
 </template>
 
 <script>
+import error404 from '@/components/error/404.vue'
+import error500 from '@/components/error/500.vue'
+
 export default {
-  layout: 'empty',
   props: {
     error: {
       type: Object,
-      default: null,
+      default: () => {},
     },
   },
-  data() {
-    return {
-      pageNotFound: '404 Not Found',
-      otherError: 'An error occurred',
-    }
-  },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title,
-    }
+  computed: {
+    errorPage() {
+      if (this.error.statusCode === 404) {
+        return error404
+      }
+      // catch everything else
+      return error500
+    },
   },
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
